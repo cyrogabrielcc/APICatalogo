fcd741955120b3cf62e5c7123a90f2cd3856edc7
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-14 20:12:08 -0300

    Lanches que eu Esqueci

--- Migrations/20230814230230_PopulandoPrdutos.cs
+++ Migrations/20230814230230_PopulandoPrdutos.cs
@@ -10,7 +10,7 @@ namespace APICatalogo.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Coca-Cola Diet', 'Refrigerante de Cola 350ml', 5.45, 'coca-cola.jpg', 35, GETDATE(), 1)");
+            migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Coca-Cola Diet', 'Refrigerante de Cola 350ml', 5.45, 'coca-cola.jpg', 35, GETDATE(), 1)");
             migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Pepsi', 'Refrigerante de Cola 350ml', 4.99, 'pepsi.jpg', 35, GETDATE(), 1 )");
             migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Guaraná Antarctica', 'Refrigerante de Guaraná 350ml', 4.75, 'guarana.jpg', 35, GETDATE(), 1)");
             migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Bolo de Chocolate', 'Delicioso bolo de chocolate', 15.99, 'bolo-chocolate.jpg', 35, GETDATE(), 2)");
---
+++ Migrations/20230814230659_PopulandoProdutosLanches.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulandoProdutosLanches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Batata Frita', 'Porção de batata frita crocante', '5.25', 'batata-frita.jpg', 35, GETDATE(), 3 )" );
+            migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Sanduíche de Frango', 'Sanduíche de frango grelhado com alface e maionese', '8.75', 'sanduiche-frango.jpg', 35, GETDATE(), 3 )");
+            migrationBuilder.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Salada Caesar', 'Salada fresca com alface, frango e molho Caesar', '7.99', 'salada-caesar.jpg', 35, GETDATE(), 3 )");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Produtos");
+        }
+    }
+}
