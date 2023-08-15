85c8655832895a931273ed2943c28ab7babd0b1c
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-14 21:47:18 -0300

    Retorna todas os produtos

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APICatalogo.Context;
+using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalogo.Controllers
@@ -10,6 +12,22 @@ namespace APICatalogo.Controllers
     [Route("api/[controller]")]
     public class ProdutosController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // -------------- Retornando todos os produtos --------------
+        [HttpGet]
+        public  ActionResult<IEnumerable<Produto>> Get(){
+            if(_context.Produtos.ToList() is null)
+            {
+                return NotFound();
+            }
+            return _context.Produtos.ToList();
+        }
+
         
+
     }
 }
