67f59987d76658be1e46d0aedf6c55abbc2d7cd2
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-14 22:38:17 -0300

    DELETE : Exclui um produt

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -81,12 +81,14 @@ namespace APICatalogo.Controllers
         }
         // -------------- Deletando um produto --------------
 
+        [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(produto => produto.ProdutoId == id);
             if (produto is null) {return NotFound();}
 
             _context.Produtos.Remove(produto);
+            _context.SaveChanges();
             return Ok(produto);
 
         }
