2bbae3b63a040927b986708c27f9051c0c392b4a
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-21 21:26:23 -0300

    90. Data Annotations

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,8 +1,3 @@
-using System.Net;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using APICatalogo.Context;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
