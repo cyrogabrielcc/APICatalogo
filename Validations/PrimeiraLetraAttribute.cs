7c4049774aaf8211697006fd3fe94dc6cc0862e4
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-10-01 17:33:37 -0300

    iniciando Aula 91

--- Validations/PrimeiraLetraAttribute.cs
+++ Validations/PrimeiraLetraAttribute.cs
@@ -1,9 +1,21 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Validations
 {
-    public class PrimeiraLetraAttribute
+    public class PrimeiraLetraAttribute : ValidationAttribute
     {
-        
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Validando se não é nulo
+            if (value == null || string.IsNullOrEmpty(value.ToString())) return ValidationResult.Success;
+            
+            // Vendo se a Primeira letra é maiuscula
+            var pri = value.ToString()[0].ToString();
+            
+            // Fazendo a validação
+            if (pri != pri.ToUpper()) return new ValidationResult("Deve iniciar com maiúscu");
+            
+            return base.IsValid(value, validationContext);
+        }
     }
 }
